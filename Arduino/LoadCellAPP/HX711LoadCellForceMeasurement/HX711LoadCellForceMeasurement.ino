1cf87910e67bcb80b31729b77fab24cf42067d31
INBV <68478358+inbv@users.noreply.github.com>
2020-07-18 14:27:10 -0500

    Added Arduino code and readme

---
+++ Arduino/LoadCellAPP/HX711LoadCellForceMeasurement/HX711LoadCellForceMeasurement.ino
@@ -0,0 +1,52 @@
+/*
+ * https://circuits4you.com
+ * 2016 November 25
+ * Load Cell HX711 Module Interface with Arduino to measure weight in Kgs
+ Arduino 
+ pin 
+ 2 -> HX711 CLK
+ 3 -> DOUT
+ 5V -> VCC
+ GND -> GND
+ 
+ Most any pin on the Arduino Uno will be compatible with DOUT/CLK.
+ The HX711 board can be powered from 2.7V to 5V so the Arduino 5V power should be fine.
+*/
+ 
+#include "HX711.h"  //You must have this library in your arduino library folder
+ 
+#define DOUT  3
+#define CLK  2
+ 
+HX711 scale(DOUT, CLK);
+ 
+//Change this calibration factor as per your load cell once it is found you many need to vary it in thousands
+float calibration_factor = -1066560.00; //-106600 worked for my 40Kg max scale setup 
+ 
+//=============================================================================================
+//                         SETUP
+//=============================================================================================
+void setup() {
+  Serial.begin(9600);  
+  Serial.println("Press T to tare");
+  scale.set_scale(-1066650.00);  //Calibration Factor obtained from first sketch
+  scale.tare();             //Reset the scale to 0  'guo
+}
+ 
+//=============================================================================================
+//                         LOOP
+//=============================================================================================
+void loop() {
+  //Serial.print("Weight: ");
+  Serial.println(scale.get_units()*1000, 0);  //Up to 3 decimal points *** VALUE MULTIPLIED BY 1000!! made to grams -- for better visuals
+  //Serial.println(" g"); //Change this to kg and re-adjust the calibration factor if you follow lbs
+ 
+  if(Serial.available())
+  {
+    char temp = Serial.read();
+    if(temp == 't' || temp == 'T')
+      scale.tare();  //Reset the scale to zero      
+  }
+  
+}
+//=============================================================================================
---
+++ Arduino/readme.md
@@ -0,0 +1,14 @@
+# DESCRIPTION
+## Purpose
+
+To calibrate/tare and read the measurements from a 1 kg load cell. The load cell is used to find the lift force(s) of different airfoils which are attached at a certain angle of attack.
+
+## Library
+In this code, the first step was to download and install the ADC HX711 [library](https://circuits4you.com/wp-content/uploads/2016/11/HX711-master.zip) **HX711-master** into the Arduino IDE. 
+
+## Application (APP)
+* Calibration - separate piece of code yielding coeffs used by measurment APP
+* Taring - embedded into measurment APP
+* Measurement
+
+Link to website with original circuit, calibration and measurement/taring code [here](https://circuits4you.com/2016/11/25/hx711-arduino-load-cell/)
