5d8a191d508d1403fbd64f776e82e9af55ac4ac9
INBV <68478358+inbv@users.noreply.github.com>
2020-11-28 11:10:33 -0600

    Octave code updated with notes

--- Octave/LiftDataProcessing/LargeAngle/LiftDataProcessingL.m
+++ Octave/LiftDataProcessing/LargeAngle/LiftDataProcessingL.m
@@ -12,7 +12,7 @@
 #
 # AUTHORSHIP
 # Ivan B., 68478358+inbv@users.noreply.github.com
-# Script Version: 1.7.5L
+# Script Version: 1.7.6L
 #    
 # Date: 16-oct-2020 7:32
 #
@@ -24,103 +24,48 @@ close all force   # close all figures
 clear             # clean RAM
 
 # NOTE: files ARE w/o extensions
-run01= '6L.txt';        # my data file name, put data file in space!
-run02= '14L2516.txt';        # 20Jul is 
+run01= '6L.txt';        # datasets for each run
+run02= '14L2516.txt';         
 run03= '13L52.txt';
 run04= '9L.txt';
 run05= '10L.txt';
 
 # load my data
 
-f1= load(run01); 
+f1= load(run01);        # load datasets
 f2= load(run02);
 f3= load(run03);
 f4= load(run04);
 f5= load(run05);  
 
 ################################################################################
-#RAW DATA
-
-####### T E M P --- Finding boundaries for each dataset
 
 #figure(1)
 
-#plot(f1(145:225))
+#plot(f1(145:225))      # Temporary figure to find out the range of proper analyzeable datapoints (i.e. non-zero)
 #plot(f2(170:230))
 #plot(f3(115:300))
 #plot(f4(130:210))
 #plot(f5(130:240))
 
-#grid
-
-
-####### T E M P
-
-
-
-
-#figure(1)  # below is figure 1 (raw data) C H A N G E TO FIGURE 1 AFTER TEMP DONE   
-
-#subplot(511)
-#plot(f1)
-#title ('Run 1 Lift Force Data')
-#xlabel('Sample Number')
-#ylabel('Lift Force, g, (+/-) 0.5g')
-#grid
-
-#subplot(512)
-#plot(f2)
-#title ('Run 2 Lift Force Data')
-#xlabel('Sample Number')
-#ylabel('Lift Force, g, (+/-) 0.5g')
-#grid
-
-#subplot(513)
-#plot(f3)
-#title ('Run 3 Lift Force Data')
-#xlabel('Sample Number')
-#ylabel('Lift Force, g, (+/-) 0.5g')
-#grid
-
-#subplot(514)
-#plot(f4)
-#title ('Run 4 Lift Force Data')
-#xlabel('Sample Number')
-#ylabel('Lift Force, g, (+/-) 0.5g')
-#grid
-
-#subplot(515)
-#plot(f5)
-#title ('Run 5 Lift Force Data')
-#xlabel('Sample Number')
-#ylabel('Lift Force, g, (+/-) 0.5g')
-#grid
-
 ################################################################################
 
-# DATA EXPLORATION and chosen ?dataset?
-
-# 'magic number' came from manual data exploration using command window
-# it is only possible using my eyes first
-
-#idx= 1:700;     # this index represents the values that you want to process
-                  # in my case, the values before 400 and after 900 are 0
-
- f1b= f1(135:246);
+ f1b= f1(135:246);      # range of proper data (i.e, non-zero)
  f2b= f2;
  f3b= f3;
  f4b= f4(140:252);
  f5b= f5(226:311);
  
+################################################################################ 
  
-figure(1)
+figure(1)              # This first figure displays the rough graph for each dataset.
 
 subplot(511)
 plot(f1b)
 title ('1:1 L Lift Force')
 xlabel('Sample Number')
 ylabel('Lift Force, g, (+/-) 1g')
-axis([0 111 2 20])
+axis([0 111 2 20])     # the axis can be set for eacth dataset
 grid
 
 subplot(512)
@@ -156,18 +101,16 @@ axis([0 85 11 35])
 grid
 
 ################################################################################
-### PROCESSED data vvv
 
-s1b= 1:length(f1b);     #NOT NECESSARY ANYMORE, BUT DOESN'T MESS WITH CODE
+s1b= 1:length(f1b);     # This part is not necessary anymore, but it is easier to leave it, as it does not mess with the code
 s2b= 1:length(f2b);
 s3b= 1:length(f3b);
 s4b= 1:length(f4b);
 s5b= 1:length(f5b);
 
+################################################################################
 
-#means:
-
-mf1 = mean(f1b); 
+mf1 = mean(f1b);                       # finds and sets mean force
 mf1v= mf1 * ones(1, length(s1b));      # this makes the mean line for the graph ( the v stands for vector).
 
 mf2 = mean(f2b); 
@@ -184,15 +127,14 @@ mf5v= mf5 * ones(1, length(s5b));
 
 
 ################################################################################
-### BELOW is standard deviation (std) and SNR (signal-to-noise ratio):
 
-std1= std(f1b);
+std1= std(f1b);         # finding and stating standard deviation
 std2= std(f2b);
 std3= std(f3b);
 std4= std(f4b);
 std5= std(f5b);
 
-snr1= mf1/std1;
+snr1= mf1/std1;         # calculating and setting signal-to-noise ratio (SNR)
 snr2= mf2/std2;
 snr3= mf3/std3;
 snr4= mf4/std4;
@@ -202,14 +144,13 @@ snr5= mf5/std5;
 
 # plot refined data (tidy data) on a new plot
 
-figure(2)
+figure(2)                        # plots superimposed datasets with mean lines, color-coded
 plot(f1b, 'r', mf1v, 'r', 
    f2b, 'g', mf2v, 'g', 
    f3b, 'b', mf3v, 'b',
    f4b, 'k', mf4v, 'k',
    f5b, 'm', mf5v, 'm')
-axis ([0 120 0 35])  # I want to set such an axis for all my 5 plots. 
-                     # the mean includes all the values of each dataset.
+axis ([0 120 0 35])              # sets axes for all 5 datasets
 xlabel ('Sample Number')
 ylabel ('Lift Force, g, (+/-) 1g')
 title('Large AOK | Red: 1:1  Green: 25:16  Blue: 5:2 Black: 4:1  Magenta: 6:1')
@@ -217,14 +158,13 @@ grid
 
 ################################################################################
 
+figure(3)                   # plots histograms (in subplots) for each dataset
 
-figure(3) #below are histograms
-
-histaxis= ([0 35 0 50]);    # change this to whichever axis you want for the histograms
+histaxis= ([0 35 0 50]);    # sets geneeral axis used for all histograms, so they are comparable
 
 subplot(511)
 hist(f1b)
-axis(histaxis)   #axis for all histograms; makes them visually comparable
+axis(histaxis)  
 title('1:1 L Histogram')
 xlabel('Lift Force, g, (+/-) 1g')
 ylabel ('Number of Samples')
@@ -263,14 +203,13 @@ ylabel ('Number of Samples')
 grid
 
 ################################################################################
-### Final summary "message" vv
-
+### Final summary "message" 
 
 mfmessage= 'mean forces:';
 stdmessage= 'standard deviation:';
 snrmessage= 'SNR(signal-to-noise ratio):';
 
-disp('(1:1, 25:16, 5:2, 4:1, 6:1)')
+disp('(1:1, 25:16, 5:2, 4:1, 6:1)')       # displays mean forces, standard deviations, and SNR
 disp(mfmessage)
 disp([mf1 mf2 mf3 mf4 mf5])
 
@@ -282,11 +221,11 @@ disp([snr1 snr2 snr3 snr4 snr5])
 
 ################################################################################
 
-fig4mx=([1 25/16 5/2 4 6]);
-fig4axis= ([0 7 0 30]);
+fig4mx=([1 25/16 5/2 4 6]);               # final figure (4; below) plots mean force vs. thickness ratio (in decimal form)
+fig4axis= ([0 7 0 30]);                   # refer to LiftDataProcessingA for more detail on how this works. It is much more simple than the other code.
 fig4plot= ([mf1 mf2 mf3 mf4 mf5]);
 
-figure(4)                           ########### W O R K   O N     T H I S
+figure(4)                          
 
 plot(fig4mx, fig4plot, fig4mx, fig4plot, 'bo')
 axis(fig4axis)
--- Octave/LiftDataProcessing/LiftDataProcessingA.m
+++ Octave/LiftDataProcessing/LiftDataProcessingA.m
@@ -1,18 +1,14 @@
 # -------------------------------------------------------------------
 # IB Physics Extended Essay
-# LiftDataProcessing
+# LiftDataProcessingA
 #
 # DESCRIPTION
-#  - visualize lift force data of all runs in both raw form and comparable,
-#  processed form. 
-#  - find their mean, standard deviation, and signal-to-noise ration (SNR)
-#    - display these results in a summery text message
+#  - Compare 2 datasets in a visual way, with uncertainties
 #
-# REFERENCES
 #
 # AUTHORSHIP
 # Ivan B., 68478358+inbv@users.noreply.github.com
-# Script Version: 1.7.5M
+# Script Version: 1.6.9A
 #    
 # Date: 16-oct-2020 7:32
 #
@@ -23,22 +19,22 @@
 close all force   # close all figures
 clear             # clean RAM
 
-# NOTE: files ARE w/o extensions
-run01= 'MediumAngle/1M.txt';        # my data file name, put data file in space!
-run02= 'MediumAngle/17M2516.txt';        # 20Jul is 
+run01= 'MediumAngle/1M.txt';        # put datasets from the first set here. In my case, this is the Medium Angle of Attack (AOK)
+run02= 'MediumAngle/17M2516.txt';        
 run03= 'MediumAngle/16M52.txt';
 run04= 'MediumAngle/4M.txt';
 run05= 'MediumAngle/5M.txt';
 
 ###
 
-run06='LargeAngle/6L.txt';
+run06='LargeAngle/6L.txt';          # put datasets from the second set here. In my case, this is the Large Angle of Attack (AOK)
 run07='LargeAngle/14L2516.txt';
 run08='LargeAngle/13L52.txt';
 run09='LargeAngle/9L.txt';
 run10='LargeAngle/10L.txt';
 
-# load my data
+################################################################################
+# load files
 
 f1= load(run01); 
 f2= load(run02);
@@ -52,8 +48,9 @@ f8= load(run08);
 f9= load(run09);
 f10= load(run10);
 
+################################################################################
 
- f1b= f1(145:225);
+ f1b= f1(145:225);                  # Adjust the range for each dataset here. i.e, the set of datapoints which are valid
  f2b= f2;
  f3b= f3;
  f4b= f4(130:210);
@@ -65,7 +62,9 @@ f10= load(run10);
  f9b= f9(140:252);
  f10b= f10(226:311);
  
- mf1 = mean(f1b);
+################################################################################
+
+ mf1 = mean(f1b);                   # find the mean average for each dataset. 
  mf2 = mean(f2b);
  mf3 = mean(f3b);
  mf4 = mean(f4b);
@@ -76,46 +75,27 @@ f10= load(run10);
  mf8 = mean(f8b);
  mf9 = mean(f9b);
  mf10 = mean(f10b);
- 
- 
-
-
-################################################################################
-### Final summary "message" vv
-
-
-#mfmessage= 'mean forces:';
-#stdmessage= 'standard deviation:';
-#snrmessage= 'SNR(signal-to-noise ratio):';
-
-#disp('(1:1, 25:16, 5:2, 4:1, 6:1)')
-#disp(mfmessage)
-#disp([mf1 mf2 mf3 mf4 mf5])
-
-#disp(stdmessage)
-#disp([std1 std2 std3 std4 std5])
-
-#disp(snrmessage)
-#disp([snr1 snr2 snr3 snr4 snr5])
 
 ################################################################################
 
-fig4mx=([1 25/16 5/2 4 6]);
-fig4axis= ([0 7 0 35]);
-fig4plot1= ([mf1 mf2 mf3 mf4 mf5]);
-fig4plot2= ([mf6 mf7 mf8 mf9 mf10]);
+fig4mx=([1 25/16 5/2 4 6]);                 # state x-axis values. In this case, it is the thickness ratio in decimal form
+fig4axis= ([0 7 0 35]);                     # state range of x-axis to include all the values in fig4max
+fig4plot1= ([mf1 mf2 mf3 mf4 mf5]);         # state the y-values for the first dataset. In this case, it would be the lift force of the Medium AOK.
+fig4plot2= ([mf6 mf7 mf8 mf9 mf10]);        # state the y-values for the seecond dataset. In this case, it would be the lift force of the Large AOK.
 
-err= ([5 4 4 3 2]);
+err= ([5 4 4 3 2]);                         # state the uncertainty for each x-value. Not that this uncertainty is on the y-axis, and is both above and below the datapoint, and will apply to both datasets.
 
-t1= fig4plot1 + err/2;
-b1= fig4plot1 - err/2;
+t1= fig4plot1 + err/2;                      # states the (maximum) uncertainty above each datapoint for the first dataset (i.e, M AOK)
+b1= fig4plot1 - err/2;                      # states the (minimum) uncertainty below each datapoint for the first dataset (i.e, M AOK)
 
-t2= fig4plot2 + err/2;
-b2= fig4plot2 - err/2;
+t2= fig4plot2 + err/2;                      # states the (maximum) uncertainty above each datapoint for the second dataset (i.e, L AOK)
+b2= fig4plot2 - err/2;                      # states the (minimum) uncertainty below each datapoint for the second dataset (i.e, L AOK)
 
-figure(1)                           ########### W O R K   O N     T H I S
+################################################################################
 
-plot(fig4mx, fig4plot1, 'r-', fig4mx, fig4plot1, 'ro', fig4mx, fig4plot2, 'b:', fig4mx, fig4plot2, 'bo',
+figure(1)                           
+
+plot(fig4mx, fig4plot1, 'r-', fig4mx, fig4plot1, 'ro', fig4mx, fig4plot2, 'b:', fig4mx, fig4plot2, 'bo',            # the next is complicated. Summarized, it displays the final plot. As long as the values in the previous section are correct, this figure should turn out fine.
 fig4mx, t1, 'r--', fig4mx, b1, 'r--', fig4mx, t2, 'b--', fig4mx, b2, 'b--')
 
 hold on
@@ -130,8 +110,8 @@ for i=1:length(fig4mx)
 end
 hold off
 
-axis(fig4axis)
-title('Mean Force vs. Airfoil Model , Red(-): M AOK , Blue (...): L AOK')
+axis(fig4axis)                          
+title('Mean Force vs. Airfoil Model , Red(-): M AOK , Blue (...): L AOK')           #consider changing these values for you own purposes. 
 xlabel('Top to Bottom Thickness Ratio')
 ylabel('Mean Force, g')
 grid
